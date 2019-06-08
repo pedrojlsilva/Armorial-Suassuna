09c21aab9d2bdb05b19d3b27fc58f58771015885
zsmn <zsmn@cin.ufpe.br>
2019-06-08 10:27:34 -0300

    implementacao do kalman filter para os robos azuis

--- visao/main.cpp
+++ visao/main.cpp
@@ -13,6 +13,7 @@
 #include "Ball.hpp"
 #include "Loss.hpp"
 #include "Noise.hpp"
+#include "kalman.hpp"
 
 #define PORT 8881
 #define IP "127.0.0.1"
@@ -37,9 +38,117 @@ typedef struct{
     pair<double, double> ball;
 } pacote;
 
+/* funções do kalman */
+KalmanFilter::KalmanFilter(
+    double dt,
+    const Eigen::MatrixXd& A,
+    const Eigen::MatrixXd& C,
+    const Eigen::MatrixXd& Q,
+    const Eigen::MatrixXd& R,
+    const Eigen::MatrixXd& P)
+  : A(A), C(C), Q(Q), R(R), P0(P),
+    m(C.rows()), n(A.rows()), dt(dt), initialized(false),
+    I(n, n), x_hat(n), x_hat_new(n)
+{
+  I.setIdentity();
+}
+
+KalmanFilter::KalmanFilter() {}
+
+void KalmanFilter::init(double t0, const Eigen::VectorXd& x0) {
+  x_hat = x0;
+  P = P0;
+  this->t0 = t0;
+  t = t0;
+  initialized = true;
+}
+
+void KalmanFilter::init() {
+  x_hat.setZero();
+  P = P0;
+  t0 = 0;
+  t = t0;
+  initialized = true;
+}
+
+void KalmanFilter::update(const Eigen::VectorXd& y) {
+
+  if(!initialized)
+    throw std::runtime_error("Filter is not initialized!");
+
+  x_hat_new = A * x_hat;
+  P = A*P*A.transpose() + Q;
+  K = P*C.transpose()*(C*P*C.transpose() + R).inverse();
+  x_hat_new += K * (y - C*x_hat_new);
+  P = (I - K*C)*P;
+  x_hat = x_hat_new;
+
+  t += dt;
+}
+void KalmanFilter::update(const Eigen::VectorXd& y, double dt, const Eigen::MatrixXd A) {
+
+  this->A = A;
+  this->dt = dt;
+  update(y);
+}
+void KalmanFilter::update(const Eigen::VectorXd& y, double dt, const Eigen::MatrixXd A, const Eigen::MatrixXd Q) {
+
+  this->A = A;
+  this->Q = Q;
+  this->dt = dt;
+  update(y);
+}
+
+/* funções do kalman */
+
 mutex info_mutex;
 unsigned int filterTime;
 bool proc1 = false;
+
+vector<KalmanFilter> kFiltersXB;
+vector<KalmanFilter> kFiltersYB;
+
+vector<KalmanFilter> kFiltersXY;
+vector<KalmanFilter> kFiltersYY;
+
+bool firstKalmanInputB[maxRobots] = {false};
+bool firstKalmanInputY[maxRobots] = {false};
+
+
+void initializeKalman(){
+    double dt = 1.0/30; // Time step
+
+    Eigen::MatrixXd A(3, 3); // System dynamics matrix
+    Eigen::MatrixXd C(1, 3); // Output matrix
+    Eigen::MatrixXd Q(3, 3); // Process noise covariance
+    Eigen::MatrixXd R(1, 1); // Measurement noise covariance
+    Eigen::MatrixXd P(3, 3); // Estimate error covariance
+
+    // Discrete LTI projectile motion, measuring position only
+    A << 1, dt, dt*dt/2, 0, 1, dt, 0, 0, 1;
+    C << 1, 0, 0;
+
+    // Reasonable covariance matrices
+    Q << dt*dt*dt*dt/4, dt*dt*dt/2, dt*dt/2, dt*dt*dt/2, dt*dt, dt, dt*dt/2, dt, 1;
+    R << 0.001;
+    P << 1, 0, 0, 0, 1, 0, 0, 0, 1;
+
+    std::cout << "A: \n" << A << std::endl;
+    std::cout << "C: \n" << C << std::endl;
+    std::cout << "Q: \n" << Q << std::endl;
+    std::cout << "R: \n" << R << std::endl;
+    std::cout << "P: \n" << P << std::endl;
+
+    for(int x = 0; x < maxRobots; x++){
+        firstKalmanInputB[x] = false;
+        firstKalmanInputY[x] = false;
+        kFiltersXB.push_back(KalmanFilter(dt,A, C, Q, R, P));
+        kFiltersYB.push_back(KalmanFilter(dt,A, C, Q, R, P));
+        kFiltersXY.push_back(KalmanFilter(dt,A, C, Q, R, P));
+        kFiltersYY.push_back(KalmanFilter(dt,A, C, Q, R, P));
+    }
+
+}
 
 /* loss filter methods */
 
@@ -87,6 +196,8 @@ void setFilterTime(unsigned int time){
     filterTime = time;
 }
 
+int glob = 0;
+
 void setRobotsInfo(SSL_DetectionFrame &detection, vector<Robot> &robosAzuis, vector<Robot> &robosAmarelos, pacote *robotsInfo){
     uint8_t qt_robosAzuis = detection.robots_blue_size();
     uint8_t qt_robosAmarelos = detection.robots_yellow_size();
@@ -110,14 +221,32 @@ void setRobotsInfo(SSL_DetectionFrame &detection, vector<Robot> &robosAzuis, vec
         robosAzuis[x].setHeight(detection.robots_blue(x).height());
         robosAzuis[x].setCoordinates(detection.robots_blue(x).x(), detection.robots_blue(x).y());
         robosAzuis[x].setAngle(detection.robots_blue(x).orientation());
+        robot_aux_coord.id = robosAzuis[x].robot_id;
+        robot_aux_coord.angle = robosAzuis[x].getAngle();
         noiseBlueRobots[x].noiseRobotFilter(robosAzuis[x]);
         lossBlueRobots[x].lossRobotFilter(robosAzuis[x]);
-        robot_aux_coord.id = robosAzuis[x].robot_id;
-        robot_aux_coord.x = robosAzuis[x].getRobotX();
-        robot_aux_coord.y = robosAzuis[x].getRobotY();
-        robot_aux_coord.angle = robosAzuis[x].getAngle();
-        (*robotsInfo).robots_blue[x] = robot_aux_coord;
+        if(firstKalmanInputB[robosAzuis[x].robot_id] == false){
+            firstKalmanInputB[robosAzuis[x].robot_id] = true;
+            Eigen::VectorXd x0(3);
+            x0 << robosAzuis[x].getRobotX(),0,0;
+            kFiltersXB[robosAzuis[x].robot_id].init(0,x0);
+            Eigen::VectorXd x1(3);
+            x1 << robosAzuis[x].getRobotY(),0,0;
+            kFiltersYB[robosAzuis[x].robot_id].init(0,x1);
+        }else{
+            Eigen::VectorXd zz(1);
+            zz << robosAzuis[x].getRobotX();
+            kFiltersXB[robosAzuis[x].robot_id].update(zz);
+            Eigen::VectorXd zy(1);
+            zy << robosAzuis[x].getRobotY();
+            kFiltersYB[robosAzuis[x].robot_id].update(zy);
+        }
+        robot_aux_coord.x = kFiltersXB[robosAzuis[x].robot_id].state().transpose()(0);
+        robot_aux_coord.y = kFiltersYB[robosAzuis[x].robot_id].state().transpose()(0);
         //robosAzuis[x].printRobotInfo();
+        cout << "chegou: " << detection.robots_blue(x).x() << " " << detection.robots_blue(x).y() << endl;
+        cout << robot_aux_coord.x << " " << robot_aux_coord.y << endl;
+        (*robotsInfo).robots_blue[x] = robot_aux_coord;
     }
     for(uint8_t x = 0; x < qt_robosAmarelos; x++){
         robosAmarelos[x].setId(detection.robots_yellow(x).robot_id());
@@ -149,6 +278,7 @@ void setBallInfo(SSL_DetectionFrame &detection, Ball &ball, pacote *robotsInfo){
 
 int main(){
     setFilterTime(tempoFiltro);
+    initializeKalman();
     vector<Robot> robosAzuis;
     vector<Robot> robosAmarelos;
     /* criação do vetor dinamico de objetos */
@@ -182,7 +312,7 @@ int main(){
     // setando o endereco e as portas
     address.sin_family = AF_INET; 
     address.sin_addr.s_addr = INADDR_ANY; 
-    address.sin_port = htons( PORT ); 
+    address.sin_port = htons(PORT); 
        
     // tentando bindar o endereco (pegar prioridade) 
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0){ 
@@ -208,7 +338,7 @@ int main(){
             cout << "randomizei: " << (int)robosInfo.battery[x][y] << endl;
         }
     }
-
+    
     while(true){
         if(client.receive(packet)){
             if(packet.has_detection()){
